e1f4c8c38c955065763f7521d5a5302313386f25
masinin-elite <masinin@elite.com>
2024-10-31 14:35:55 +0000

    Fixed error installing of Upgrade DACPAC if version 10.1.0 is already installed (ADO-47314)

--- src/Database/MatterSphere_Upgrade/MatterSphere_Upgrade/Scripts/PostDeployment/43500_DocuSign_AddConstraints.sql
+++ src/Database/MatterSphere_Upgrade/MatterSphere_Upgrade/Scripts/PostDeployment/43500_DocuSign_AddConstraints.sql
@@ -1,11 +1,13 @@
-﻿IF EXISTS (SELECT * FROM sys.tables WHERE schema_id =  schema_id('config') AND name = 'dbFile')
-    ALTER TABLE [dbo].[dbDocuSignEnvelopes] ADD CONSTRAINT [FK_dbDocuSignEnvelopes_dbFile] FOREIGN KEY ([fileID]) REFERENCES [config].[dbFile] ([fileID]) ON UPDATE CASCADE ON DELETE CASCADE NOT FOR REPLICATION
-ELSE
-    ALTER TABLE [dbo].[dbDocuSignEnvelopes] ADD CONSTRAINT [FK_dbDocuSignEnvelopes_dbFile] FOREIGN KEY ([fileID]) REFERENCES [dbo].[dbFile] ([fileID]) ON UPDATE CASCADE ON DELETE CASCADE NOT FOR REPLICATION
+﻿IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS WHERE CONSTRAINT_NAME='FK_dbDocuSignEnvelopes_dbFile')
+    IF EXISTS (SELECT * FROM sys.tables WHERE schema_id =  schema_id('config') AND name = 'dbFile')
+        ALTER TABLE [dbo].[dbDocuSignEnvelopes] ADD CONSTRAINT [FK_dbDocuSignEnvelopes_dbFile] FOREIGN KEY ([fileID]) REFERENCES [config].[dbFile] ([fileID]) ON UPDATE CASCADE ON DELETE CASCADE NOT FOR REPLICATION
+    ELSE
+        ALTER TABLE [dbo].[dbDocuSignEnvelopes] ADD CONSTRAINT [FK_dbDocuSignEnvelopes_dbFile] FOREIGN KEY ([fileID]) REFERENCES [dbo].[dbFile] ([fileID]) ON UPDATE CASCADE ON DELETE CASCADE NOT FOR REPLICATION
 GO
 
-IF EXISTS (SELECT * FROM sys.tables WHERE schema_id =  schema_id('config') AND name = 'dbDocument')
-    ALTER TABLE [dbo].[dbDocuSignEnvelopes] ADD CONSTRAINT [FK_dbDocuSignEnvelopes_dbDocument] FOREIGN KEY ([envDocID]) REFERENCES [config].[dbDocument] ([docID]) ON UPDATE CASCADE ON DELETE SET NULL NOT FOR REPLICATION
-ELSE
-    ALTER TABLE [dbo].[dbDocuSignEnvelopes] ADD CONSTRAINT [FK_dbDocuSignEnvelopes_dbDocument] FOREIGN KEY ([envDocID]) REFERENCES [dbo].[dbDocument] ([docID]) ON UPDATE CASCADE ON DELETE SET NULL NOT FOR REPLICATION
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS WHERE CONSTRAINT_NAME='FK_dbDocuSignEnvelopes_dbDocument')
+    IF EXISTS (SELECT * FROM sys.tables WHERE schema_id =  schema_id('config') AND name = 'dbDocument')
+        ALTER TABLE [dbo].[dbDocuSignEnvelopes] ADD CONSTRAINT [FK_dbDocuSignEnvelopes_dbDocument] FOREIGN KEY ([envDocID]) REFERENCES [config].[dbDocument] ([docID]) ON UPDATE CASCADE ON DELETE SET NULL NOT FOR REPLICATION
+    ELSE
+        ALTER TABLE [dbo].[dbDocuSignEnvelopes] ADD CONSTRAINT [FK_dbDocuSignEnvelopes_dbDocument] FOREIGN KEY ([envDocID]) REFERENCES [dbo].[dbDocument] ([docID]) ON UPDATE CASCADE ON DELETE SET NULL NOT FOR REPLICATION
 GO
